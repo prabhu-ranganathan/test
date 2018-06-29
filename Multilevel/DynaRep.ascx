1787d1d8f0e209eb960fbf796e16d168d5c3df26
Prabhu.Ranganathan <Prabhu.Ranganathan@MLBTAL-12870>
2018-06-29 12:30:21 +0530

    Add project files.

---
+++ Multilevel/DynaRep.ascx
@@ -0,0 +1,126 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DynaRep.ascx.cs" Inherits="Multilevel.DynaRep" %>
+<style type="text/css">
+    /* menu item styles */
+#navcontainer
+{
+	border-left: solid 0px Gray;
+	border-Top: solid 0px Gray;
+	border-bottom: solid 1px Gray;	
+	border-right: solid 1px Gray;	
+	height: 400px;
+	width : 200px; 
+	overflow:hidden;
+}
+#menuroot
+{
+	vertical-align :middle;
+	font-family: Segoe UI, Verdana, Tahoma, Helvetica, Arial, sans-serif;
+	font-size: 11pt ;
+	list-style-type:circle;
+	list-style-position: inside; 			
+	margin: 0;
+	padding: 15px 0 0 5px;
+	color : Maroon ;
+	line-height: 2em;
+	
+} 
+#menuroot li 
+{ 
+	padding:3px;
+	text-align: left;	
+	vertical-align :middle;	
+	
+} 
+#menuroot a
+{
+	width: 150px;
+	line-height: 2em;
+	display:block;	
+	text-decoration: underline;
+	font-weight: bold;  
+	line-height: normal;
+} 
+#menuroot a:link, #menuroot a:visited
+{
+	color:Black;	
+	text-decoration: none;
+	font-weight :lighter  ;
+	font-size: 10pt ;
+	line-height: normal;
+} 
+#menuroot a:hover
+{
+	color:Maroon;  	
+	text-decoration: none;
+	font-size: 10pt ;
+	line-height: normal;
+}
+
+
+/* right hand menu second level */
+#menusecondlevel
+{
+	vertical-align :middle;
+	font-family: Segoe UI, Verdana, Tahoma, Helvetica, Arial, sans-serif;
+	list-style-type:square;
+	list-style-position: inside; 			
+	margin: 0;
+	padding: 0 0 0 10px;
+	line-height: 1.5em;
+} 
+#menusecondlevel li 
+{ 
+	padding:1px;
+	text-align: left;	
+	vertical-align :middle;
+} 
+#menusecondlevel a
+{
+	width: 150px;
+	display:block;	
+	font-weight: normal;  
+	font-size: 3pt ;
+	text-decoration: none;		
+} 
+#menusecondlevel a:link, #menusecondlevel a:visited
+{
+	color:Black;	
+} 
+#menusecondlevel a:hover
+{
+	color:Maroon;  	
+}
+</style>
+<div>
+    <asp:Repeater ID="FirstLevelMenuRepeater" runat="server" OnItemDataBound="FirstLevelMenuRepeater_ItemDataBound">
+        <HeaderTemplate>
+            <div id="navcontainer">
+                <ul id="menuroot">
+        </HeaderTemplate>
+        <ItemTemplate>
+            <li>
+                <asp:HyperLink ID="FirstLevelMenu" runat="server" NavigateUrl='<%# DataBinder.Eval(Container.DataItem, "Url") %>'
+                    Text='<%# DataBinder.Eval(Container.DataItem, "Title") %>' ToolTip='<%# DataBinder.Eval(Container.DataItem, "Desc") %>'>
+                </asp:HyperLink>
+                <asp:Repeater ID="SecondLevelMenuRepeater" runat="server" >
+                    <HeaderTemplate>
+                            <ul id="menusecondlevel">
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <li>
+                            <asp:HyperLink ID="SecondLevelMenu" runat="server" NavigateUrl='<%# DataBinder.Eval(Container.DataItem, "Url") %>'
+                                Text='<%# DataBinder.Eval(Container.DataItem, "Title") %>' ToolTip='<%# DataBinder.Eval(Container.DataItem, "Desc") %>'>
+                            </asp:HyperLink>
+                        </li>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        </ul></FooterTemplate>
+                </asp:Repeater>                
+            </li>
+        </ItemTemplate>
+        <FooterTemplate>
+                </ul>
+            </div>
+        </FooterTemplate>
+    </asp:Repeater>
+</div>
---
+++ Multilevel/DynaRep.ascx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Multilevel
+{
+    public partial class DynaRep : System.Web.UI.UserControl
+    {
+        List<MenuItem> _FirstLevelMenuItems = new List<MenuItem>();
+        List<MenuItem> SecondLevelMenuItems = new List<MenuItem>();
+        List<MenuItem> _SecondLevelMenuItems1 = new List<MenuItem>();
+        List<MenuItem> _SecondLevelMenuItems2 = new List<MenuItem>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            _FirstLevelMenuItems.Clear();
+            _SecondLevelMenuItems1.Clear();
+            _SecondLevelMenuItems2.Clear();
+
+            //Create first level menu items.
+            MenuItem mi1 = new MenuItem("First Level -  Menu item 1", "First Level -  Menu item 1", "#Menu1");
+            MenuItem mi2 = new MenuItem("First Level -  Menu item 2", "First Level -  Menu item 2", "#Menu2");
+            MenuItem mi3 = new MenuItem("First Level -  Menu item 3", "First Level -  Menu item 3", "#Menu3");
+            MenuItem mi4 = new MenuItem("First Level -  Menu item 4", "First Level -  Menu item 4", "#Menu4");
+
+            _FirstLevelMenuItems.Add(mi1);
+            _FirstLevelMenuItems.Add(mi2);
+            _FirstLevelMenuItems.Add(mi3);
+            _FirstLevelMenuItems.Add(mi4);
+
+            //Create second level menu items.
+            MenuItem submenuitem1 = new MenuItem("Second Level -  Menu item 1", "Second Level -  Menu item 1", "#SubMenu1");
+            MenuItem submenuitem2 = new MenuItem("Second Level -  Menu item 2", "Second Level -  Menu item 2", "#SubMenu2");
+            MenuItem submenuitem3 = new MenuItem("Second Level -  Menu item 3", "Second Level -  Menu item 3", "#SubMenu3");
+            MenuItem submenuitem4 = new MenuItem("Second Level -  Menu item 4", "Second Level -  Menu item 4", "#SubMenu4");
+
+            _SecondLevelMenuItems1.Add(submenuitem1);
+            _SecondLevelMenuItems1.Add(submenuitem2);
+            _SecondLevelMenuItems2.Add(submenuitem3);
+            _SecondLevelMenuItems2.Add(submenuitem4);
+
+            FirstLevelMenuRepeater.DataSource = _FirstLevelMenuItems;
+            FirstLevelMenuRepeater.DataBind();
+        }
+
+
+
+        private class MenuItem
+        {
+            string _Title;
+            string _Desc;
+            string _Url;
+
+            public MenuItem()
+            {
+            }
+            public MenuItem(string title, string desc, string Url)
+            {
+                _Title = title;
+                _Desc = desc;
+                _Url = Url;
+            }
+
+            public string Title
+            {
+                get
+                {
+                    return _Title;
+                }
+                set
+                {
+                    _Title = value;
+                }
+            }
+
+            public string Desc
+            {
+                get
+                {
+                    return _Desc;
+                }
+                set
+                {
+                    _Desc = value;
+                }
+            }
+
+            public string Url
+            {
+                get
+                {
+                    return _Url;
+                }
+                set
+                {
+                    _Url = value;
+                }
+            }
+
+        }
+
+        protected void FirstLevelMenuRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.DataItem != null)
+            {
+                string Title = DataBinder.Eval(e.Item.DataItem, "Title").ToString().Trim();
+                SecondLevelMenuItems.Clear();
+                GetSubMenuItems(Title, SecondLevelMenuItems);
+                Repeater FourthLevelMenuRepeater = (Repeater)e.Item.FindControl("SecondLevelMenuRepeater");
+                FourthLevelMenuRepeater.DataSource = SecondLevelMenuItems;
+                FourthLevelMenuRepeater.DataBind();
+            }
+        }
+
+        private void GetSubMenuItems(string Title, List<MenuItem> MenuItems)
+        {
+            if (Title == "First Level -  Menu item 1")
+            {
+                MenuItems = _SecondLevelMenuItems1;
+            }
+            else if (Title == "First Level -  Menu item 2")
+            {
+                MenuItems = _SecondLevelMenuItems2;
+            }
+            SecondLevelMenuItems = MenuItems;
+        }
+    }
+}
